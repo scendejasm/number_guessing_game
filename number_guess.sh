4f209584b3c51c8baa2e336be02bea3ceeeb09d8
SCendejas <scendejasm@gmail.com>
2022-09-15 10:53:40 -0700

    fix: work around git push

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,31 @@
 #!/bin/bash 
+# $PSQL variable for interacting with the DB
+PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
 
+echo -e "\n~~~~ Number Guessing Game ~~~~\n"
+
+main_menu () {
+  echo Enter your username: 
+  read PLAYER
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$PLAYER';")
+  if [[ -z $USER_ID ]]
+  then
+    echo -e "\nWelcome, $PLAYER! It looks like this is your first time here."
+    guessing_game $PLAYER
+  else
+    echo -e "\nWelcome back, $PLAYER"
+    guessing_game $USER_ID 
+  fi
+}
+
+guessing_game () {
+  echo -e "\n TEST: $1 :Endgame"
+  if [[ $1 =~ '^[0-9]+$' ]]
+  then
+    echo $1 is a user_id
+  else
+    echo $1 is a username
+  fi
+}
+
+main_menu
