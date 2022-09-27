2821a8bcb72ade69e89bf3bc84cb0882a33b7de4
scendejasm <scendejasm@gmail.com>
2022-09-27 16:25:53 +0000

    fix: 'welcome back' message

--- number_guess.sh
+++ number_guess.sh
@@ -16,15 +16,18 @@ main_menu () {
     USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$PLAYER';")
     guessing_game $USER_ID
   else # Get player stats 
+    GAMES_PLAYED=$($PSQL "SELECT count(game_id) FROM GAMES WHERE user_id=$USER_ID")
+    BEST_GAME=$($PSQL "SELECT min(user_guesses) FROM GAMES WHeRE user_id=$USER_ID")
+    echo -e "\n"
+    echo "Welcome back, $PLAYER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
     
-    echo -e "\nWelcome back, $PLAYER"
     guessing_game $USER_ID 
   fi
 }
 
 guessing_game () {
   SECRET_NUMBER=$[ $RANDOM % 1000 + 1 ]
-  TRIES=1
+  NUMBER_OF_GUESSES=1
   echo -e "\n"
   echo -e "Guess the secret number between 1 and 1000:"
   echo -e "\nHINT: The secret number is $SECRET_NUMBER \n"
@@ -33,29 +36,29 @@ guessing_game () {
   do
     if ! [[ $PLAYER_GUESS =~ ^[0-9]+$ ]]
     then
-      echo -e "That is not and interger, guess again:"
-      TRIES=$((TRIES + 1))
-      echo $TRIES
+      echo -e "That is not an integer, guess again:"
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
+      #echo $NUMBER_OF_GUESSES
       read PLAYER_GUESS
     elif [ $PLAYER_GUESS -gt $SECRET_NUMBER ]
     then
       echo -e "It's lower than that, guess again:"
-      TRIES=$((TRIES + 1))
-      echo $TRIES
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
+      #echo $NUMBER_OF_GUESSES
       read PLAYER_GUESS
     elif [ $PLAYER_GUESS -lt $SECRET_NUMBER ]
     then
       echo -e "It's higher than that, guess again:"
-      TRIES=$((TRIES + 1))
-      echo $TRIES
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
+      #echo $NUMBER_OF_GUESSES
       read PLAYER_GUESS
     fi
   done
 
-  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, user_guesses, user_guessed_secret, secret_number) VALUES($1, $TRIES, True, $SECRET_NUMBER)")
-  echo $INSERT_GAME_RESULT
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, user_guesses, user_guessed_secret, secret_number) VALUES($1, $NUMBER_OF_GUESSES, True, $SECRET_NUMBER)")
+  #echo $INSERT_GAME_RESULT
   # echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+  echo -e "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
   # echo -e "\n"
 }
 
